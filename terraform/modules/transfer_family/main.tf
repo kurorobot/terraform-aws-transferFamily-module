eee77f080b0b302e855a99fea560b5cb08b7d7c4
kurorobot <kuromatsu116711@gmail.com>
2025-04-18 09:28:47 +0900

    FIX: modules/main.tfのlambda部分

--- terraform/modules/transfer_family/main.tf
+++ terraform/modules/transfer_family/main.tf
@@ -3,15 +3,12 @@ resource "aws_transfer_server" "ftp_server" {
   endpoint_type          = "VPC"
   protocols              = ["FTP"]
   identity_provider_type = "AWS_LAMBDA"
+  function = var.lambda_function_arn
 
   endpoint_details {
     vpc_id             = var.vpc_id
     security_group_ids = var.security_group_ids
     subnet_ids         = var.subnet_ids
-  }
-
-  identity_provider_details = {
-    function = var.lambda_function_arn
   }
 
   tags = var.tags
